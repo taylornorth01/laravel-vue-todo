8df7a844728a328eaac678159a5e270f8f38fbb4
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 20:59:50 +0100

    Create new task form and axios request

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,13 +1,52 @@
 <template>
-    <section>
+	<section>
+		<div>
+			<p>Task list</p>
+			<button v-on:click="showForm">Create new task</button>
 
-    </section>
+			<div v-if="isShowForm">
+				<input type="text" v-model="title">
+				<input type="text" v-model="text">
+				<button v-on:click="createTask">Submit</button>
+			</div>
+
+		</div>
+	</section>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Header component mounted.')
-        }
-    }
+	export default {
+		mounted() {
+			console.log('Header component mounted.')
+		},
+
+		data() {
+			return {
+				isShowForm: false,
+				title: '',
+				text: ''
+			}
+		},
+
+		methods: {
+			showForm() {
+				this.isShowForm = !this.isShowForm;
+			},
+
+			createTask() {
+				axios.post('/tasks', {
+					task_title: this.title, 
+					text_body: this.text
+				})
+				.then(response => console.log(response.data.message))
+				.catch(error => console.log(error));
+
+				this.title = '';
+				this.text = '';
+				this.showForm();
+
+				this.$emit('refreshList');
+			}
+		}
+	}
 </script>

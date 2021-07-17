6adcab8e8ffeca2ca8ea1c549ac168e80dc8a3bb
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 19:47:57 +0100

    Added endpoint allowing for list refreshes

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -3,7 +3,8 @@
       <header-component></header-component>
 
       <task-list-component 
-        v-bind:tasks="this.taskMeta.data">
+        v-bind:tasks="this.taskMeta.data"
+        v-on:refreshList="refreshList">
       </task-list-component>
 
       <footer-component 
@@ -15,30 +16,40 @@
 
 <script>
     export default {
-        mounted(){
-          console.log('App component mounted.');
-          axios.get('/tasks').then(response => {
+      mounted(){
+        console.log('App component mounted.');
+        axios.get(this.currentEndpoint).then(response => {
+          console.log("Response: ", response.data);
+          this.taskMeta = response.data
+        });
+      },
+
+      data() {
+        return {
+          taskMeta: [],
+          currentEndpoint: '/tasks'
+        }
+      },
+
+      methods: {
+        getPage(page) {
+          if (page !== undefined) {
+            console.log("Updating page.");
+            this.currentEndpoint = '/' + page.url.split('/').pop();
+            axios.get(this.currentEndpoint).then(response => {
+              console.log("Response: ", response.data);
+              this.taskMeta = response.data
+            });
+          }
+        },
+
+        refreshList() {
+          console.log("Refreshing list: ", this.currentEndpoint);
+          axios.get(this.currentEndpoint).then(response => {
             console.log("Response: ", response.data);
             this.taskMeta = response.data
           });
-        },
-
-        data() {
-          return {
-            taskMeta: []
-          }
-        },
-
-        methods: {
-          getPage(page) {
-            if (page !== undefined) {
-              console.log("Updating page.");
-              axios.get('/' + page.url.split('/').pop()).then(response => {
-                console.log("Response: ", response.data);
-                this.taskMeta = response.data
-              });
-            }
-          }
         }
+      }
     }
 </script>

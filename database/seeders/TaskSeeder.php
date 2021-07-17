af2ba1edd090e071a6ac19c18a7a7a6e01dcfa05
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 14:03:11 +0100

    Task seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
 class TaskSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Task::factory()
+            ->count(50)
+            ->create();
     }
 }

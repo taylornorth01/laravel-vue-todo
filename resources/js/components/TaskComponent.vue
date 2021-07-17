b025c2136ad60a338d1621e9303e0cee096d1980
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 21:50:07 +0100

    Edit task form and axios request

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -1,9 +1,15 @@
 <template>
 	<section>
     <p>{{ task.task_title }} -- {{ task.text_body }}</p>
-    <button>Edit</button>
+    <button v-on:click="showForm">Edit</button>
     <button>View</button>
     <button v-on:click="deleteTask(task.id)">Delete</button>
+
+    <div v-if="isShowForm">
+      <input type="text" v-model="title" placeholder="title">
+      <input type="text" v-model="text" placeholder="text">
+      <button v-on:click="editTask(task.id)">Submit</button>
+    </div>
   </section>
 </template>
 
@@ -15,11 +21,34 @@
 
     props: ['task'],
 
+    data() {
+			return {
+				isShowForm: false,
+				title: this.task.task_title,
+				text: this.task.text_body
+			}
+		},
+
     methods: {
+      showForm() {
+				this.isShowForm = !this.isShowForm;
+			},
+
       deleteTask(id) {
         axios.delete("/tasks/" + id)
         .then(response => console.log(response))
         .catch(error => console.log("Error destroying task: ", error));
+        this.$emit('refreshList');
+      },
+
+      editTask(id) {
+        axios.put("/tasks/" + id, {
+          task_title: this.title,
+          text_body: this.text
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log("Error destroying task: ", error));
+        this.showForm();
         this.$emit('refreshList');
       }
     }

a7d59f35d2d8640fa815c93a2c618c24ead4f22b
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 19:48:16 +0100

    Delete task event

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -3,21 +3,28 @@
     <ul>
       <li v-for="task in tasks" :key="task.id" >
         <p>{{ task.task_title }} -- {{ task.text_body }}</p>
+        <button>Edit</button>
+        <button>View</button>
+        <button v-on:click="deleteTask(task.id)">Delete</button>
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Task list component mounted.');
-        },
+  export default {
+    mounted() {
+      console.log('Task list component mounted.');
+    },
 
-        props: ['tasks'],
+    props: ['tasks'],
 
-        methods: {
-          
-        }
+    methods: {
+      deleteTask(id) {
+        axios.delete("/tasks/" + id).then(response => console.log(response));
+        this.$emit('refreshList');
+      }
+      
     }
+  }
 </script>

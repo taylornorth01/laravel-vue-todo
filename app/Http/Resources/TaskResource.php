a93b3f27d161871f40c5a2404fa699337d8c3ced
taylornorth01 <taylornorth13@hotmail.com>
2021-07-17 14:31:20 +0100

    Task json resource definition

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'task_title' => $this->title,
+            'text_body' => $this->text_body,
+            'date_created' => $this->date_created
+        ];
+    }
+}

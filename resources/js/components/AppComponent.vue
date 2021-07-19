26868902b190b7c903771abefa1ec078028262d9
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 14:49:02 +0100

    App init refactor

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -1,72 +1,37 @@
 <template>
-	<section class="main__layout">
-		<div class="control__panel">
-			<header-component 
-				v-on:redirectPage="redirectPage">
-			</header-component>
-			<footer-component 
-				v-bind:metadata="this.taskMeta.meta"
-				v-on:changePage="getPage">
-			</footer-component>
-		</div>
-		
-
-		<list-component 
-			v-bind:tasks="this.taskMeta.data"
-			v-on:refreshList="refreshList">
-		</list-component>
-
-		
-	</section>
+	<div>
+		App
+	</div>
 </template>
 
 <script>
-	import "../../css/app.css";
+export default {
+	mounted() {
+		console.log("App component mounted.");
+		this.initializeApp();
+	},
 
-	export default {
-		mounted(){
-			console.log('App component mounted.');
-			axios.get(this.currentEndpoint).then(response => {
-				console.log("Response: ", response.data);
-				this.taskMeta = response.data
-			});
-		},
+	data() {
+		return {
+			isLoading: true,
+			tasks: [],
+			meta: { links: [], data: [] }
+		};
+	},
 
-		data() {
-			return {
-				taskMeta: [],
-				currentEndpoint: '/tasks'
-			}
-		},
-
-		methods: {
-			getPage(page) {
-				if (page !== undefined) {
-					console.log("Updating page.");
-					this.currentEndpoint = '/' + page.url.split('/').pop();
-					axios.get(this.currentEndpoint).then(response => {
-						console.log("Response: ", response.data);
-						this.taskMeta = response.data
-					});
-				}
-			},
-
-			refreshList() {
-				console.log("Refreshing list: ", this.currentEndpoint);
-				axios.get(this.currentEndpoint).then(response => {
-					console.log("Response: ", response.data);
-					this.taskMeta = response.data
-				});
-			},
-
-			redirectPage() {
-				console.log("Redirecting to first page.");
-				this.currentEndpoint = "/tasks";
-				axios.get(this.currentEndpoint).then(response => {
-					console.log("Response: ", response.data);
-					this.taskMeta = response.data
-				});
-			}
+	methods: {
+		initializeApp() {
+			console.log("App loading...");
+			axios
+				.get("/tasks")
+				.then((res) => {
+					console.log("Request successful.");
+					this.tasks = res.data.data;
+					this.meta = { links: res.data.links, data: res.data.meta };
+				})
+				.catch((err) => console.error("Request failed.", err))
+				.then(() => (this.isLoading = false));
 		}
 	}
+};
 </script>

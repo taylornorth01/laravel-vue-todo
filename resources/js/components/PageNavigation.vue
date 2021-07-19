ad3edf0160c4b81665fc68c8ab46d66685870e36
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 15:47:46 +0100

    Page navigation component

---
+++ resources/js/components/PageNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div>
+			<p>Page: {{ meta.current_page }}</p>
+			<p>Tasks: {{ meta.from }}-{{ meta.to }}</p>
+			<p>Total: {{ meta.total }}</p>
+		</div>
+		<div>
+			<div @click="loadPage(links.prev)">
+				Previous
+			</div>
+			<div @click="loadPage(links.next)">
+				Next
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		console.log("Component mounted.");
+	},
+
+	props: ["links", "meta"],
+
+	methods: {
+		loadPage(link) {
+			if (link !== null) {
+				console.log("Loading new page.");
+				this.$emit("loadPage", link);
+			} else {
+				console.log("Page not avaliable.");
+			}
+		}
+	}
+};
+</script>

9056380cdb1657141ddb42106883610c1ea13e9f
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 14:57:31 +0100

    List and task components

---
+++ resources/js/components/ListComponent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<ul v-for="task in tasks" :key="task.id">
+			<task-component :task="task"></task-component>
+		</ul>
+	</div>
+</template>
+
+<script>
+import TaskComponent from "../../mock_practice/TaskComponent.vue";
+export default {
+	components: { TaskComponent },
+	mounted() {
+		console.log("List component mounted.");
+	},
+
+	props: ["tasks"]
+};
+</script>
---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,15 @@
+<template>
+	<li>
+		{{ task.task_title }}<br />{{ task.text_body }}<br />{{ task.date_created }}
+	</li>
+</template>
+
+<script>
+export default {
+	mounted() {
+		console.log("Task component mounted.");
+	},
+
+	props: ["task"]
+};
+</script>

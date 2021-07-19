c9d4bb671a8888b47f4bce68c9c41ebd6ff501d9
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 15:26:17 +0100

    Task can now be edited using a toggled form

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -1,7 +1,19 @@
 <template>
-	<li>
-		{{ task.task_title }}<br />{{ task.text_body }}<br />{{ task.date_created }}
-	</li>
+	<div>
+		<li>
+			<div v-if="!isEditVisible">
+				{{ task.task_title }} - {{ task.text_body }}<br />{{
+					task.date_created
+				}}
+				<button @click="isEditVisible = !isEditVisible">Edit</button>
+			</div>
+			<div v-else>
+				<input type="text" v-model="title" />
+				<input type="text" v-model="description" />
+				<button @click="editTask">Submit</button>
+			</div>
+		</li>
+	</div>
 </template>
 
 <script>
@@ -10,6 +22,30 @@ export default {
 		console.log("Task component mounted.");
 	},
 
-	props: ["task"]
+	data() {
+		return {
+			isEditVisible: false,
+			title: this.task.task_title,
+			description: this.task.text_body
+		};
+	},
+
+	props: ["task"],
+
+	methods: {
+		editTask() {
+			console.log("Editing task.");
+			this.isEditVisible = !this.isEditVisible;
+			axios
+				.put("/tasks/" + this.task.id, {
+					task_title: this.title,
+					text_body: this.description
+				})
+				.then((res) => {
+					console.log("Task editing successful.");
+				})
+				.catch((err) => console.error("Task editing failed.", err));
+		}
+	}
 };
 </script>

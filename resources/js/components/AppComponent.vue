ac54364353b6423ca6df85b4930945034095e3e2
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 15:48:26 +0100

    App built using components

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -1,11 +1,21 @@
 <template>
 	<div>
-		App
+		<new-task-form></new-task-form>
+		<list-component :tasks="tasks"></list-component>
+		<page-navigation
+			:links="meta.links"
+			:meta="meta.data"
+			@loadPage="requestPage"
+		></page-navigation>
 	</div>
 </template>
 
 <script>
+import ListComponent from "../../mock_practice/ListComponent.vue";
+import NewTaskForm from "./NewTaskForm.vue";
+import PageNavigation from "./PageNavigation.vue";
 export default {
+	components: { ListComponent, NewTaskForm, PageNavigation },
 	mounted() {
 		console.log("App component mounted.");
 		this.initializeApp();
@@ -24,6 +34,18 @@ export default {
 			console.log("App loading...");
 			axios
 				.get("/tasks")
+				.then((res) => {
+					console.log("Request successful.");
+					this.tasks = res.data.data;
+					this.meta = { links: res.data.links, data: res.data.meta };
+				})
+				.catch((err) => console.error("Request failed.", err))
+				.then(() => (this.isLoading = false));
+		},
+
+		requestPage(link) {
+			axios
+				.get(link)
 				.then((res) => {
 					console.log("Request successful.");
 					this.tasks = res.data.data;

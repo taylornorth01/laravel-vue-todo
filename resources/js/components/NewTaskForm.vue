ffbb285a79ba2ad8dede7fd0ee3deb98967217b6
taylornorth01 <taylornorth13@hotmail.com>
2021-07-19 15:06:23 +0100

    Create new task form component

---
+++ resources/js/components/NewTaskForm.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<input type="text" placeholder="Task title" v-model="title" />
+		<input type="text" placeholder="Task description" v-model="description" />
+		<button @click="createTask">Create task</button>
+	</div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		console.log("New task form component mounted.");
+	},
+
+	data() {
+		return {
+			title: "",
+			description: ""
+		};
+	},
+
+	methods: {
+		createTask() {
+			console.log("Creating a new task.");
+			axios
+				.post("/tasks", { task_title: this.title, text_body: this.description })
+				.then((res) => {
+					console.log("Task creation successful.");
+					this.tasks = res.data.data;
+					this.meta = { links: res.data.links, data: res.data.meta };
+				})
+				.catch((err) => console.error("Task creation failed.", err));
+		}
+	}
+};
+</script>
